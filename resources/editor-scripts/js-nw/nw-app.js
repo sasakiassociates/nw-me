f6086ec7b6a0f4a257ac7af702cfde07a5558a8d
Ken Goulding <kgoulding@sasaki.com>
2018-01-16 21:04:30 -0500

    restructure adding items to menu

--- resources/editor-scripts/js-nw/nw-app.js
+++ resources/editor-scripts/js-nw/nw-app.js
@@ -14,60 +14,76 @@ versionHandler.onSaveDataRequested(function (callback) {
     callback(getSaveData());
 });
 
+const addMenu = function (label, buttons) {
+    const subMenu = new gui.Menu();
+    menu.append(new gui.MenuItem({
+        label: label,
+        submenu: subMenu
+    }));
+    buttons.forEach(function (button, i) {
+        subMenu.append(new gui.MenuItem({
+            label: button.label,
+            click: button.click
+        }));
+    });
+};
 
-const editMenu = new gui.Menu();
-menu.append(new gui.MenuItem({
-    label: 'Edit',
-    submenu: editMenu
-}));
-
-editMenu.append(new gui.MenuItem({
-    label: 'Edit Mode',
-    key: '1',//not working...
-    modifiers: "ctrl",
-    click: function () {
-        console.log('editMode SET');
-        global.me_editMode = true;
-        const win = gui.Window.get();
-        win.reload();
-    }
-}));
-editMenu.append(new gui.MenuItem({
-    label: 'View Mode',
-    key: '2',//not working...
-    modifiers: "ctrl",
-    click: function () {
-        const data = getSaveData();
-        nwme.saveHtmlFile('.' + window.location.pathname, data, function () {
+addMenu('Edit', [
+    {
+        label: 'Edit Mode',
+        click: function () {
+            console.log('editMode SET');
+            global.me_editMode = true;
+            const win = gui.Window.get();
+            win.reload();
+        }
+    },
+    {
+        label: 'View Mode',
+        click:  function () {
+            const data = getSaveData();
+            nwme.saveHtmlFile('.' + window.location.pathname, data, function () {
+                const win = gui.Window.get();
+                global.me_editMode = false;
+                win.reload();
+            });
+        }
+    },
+    {
+        label: 'Discard Edits',
+        click: function () {
             const win = gui.Window.get();
             global.me_editMode = false;
             win.reload();
-        });
+        }
     }
-}));
-editMenu.append(new gui.MenuItem({
-    label: 'Discard Edits',
-    key: '3',//not working...
-    modifiers: "ctrl",
-    click: function () {
-        const win = gui.Window.get();
-        global.me_editMode = false;
-        win.reload();
-    }
-}));
+]);
 
-const exportMenu = new gui.Menu();
-menu.append(new gui.MenuItem({
-    label: 'Export',
-    submenu: exportMenu
-}));
-exportMenu.append(new gui.MenuItem({
-    label: 'Flatten',
-    key: '4',//not working...
-    modifiers: "ctrl",
-    click: function () {
-        //TODO
+addMenu('Browser', [
+    {
+        label: 'Back',
+        click: function () {
+            const win = gui.Window.get();
+            win.window.history.back();
+        }
+    },
+    {
+        label: 'Forward',
+        click:  function () {
+            const win = gui.Window.get();
+            win.window.history.forward();
+        }
     }
-}));
+]);
+
+addMenu('Export', [
+    {
+        label: 'Flatten',
+        click: function () {
+            //TODO
+        }
+    }
+]);
+
 
 gui.Window.get().menu = menu;
